159684495bf161604c2d46c6c3bbfa229aad747b
wytan12 <102575826+wytan12@users.noreply.github.com>
2023-12-12 19:44:40 +0800

    Delete EmotionFrontend directory

--- EmotionFrontend/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AnalysisComponent } from './analysis/analysis.component';
-
-export const routes: Routes = [
-    {path:"home", component: HomeComponent},
-    {path:"analysis", component: AnalysisComponent},
-];
