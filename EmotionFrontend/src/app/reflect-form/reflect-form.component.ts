8f47a9ba53336a3d49dc1c963585eab11d189f3a
bron322 <lebron13145@gmail.com>
2023-12-17 19:35:24 +0800

    Added reflection form

---
+++ EmotionFrontend/src/app/reflect-form/reflect-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reflect-form',
+  templateUrl: './reflect-form.component.html',
+  styleUrl: './reflect-form.component.css'
+})
+export class ReflectFormComponent {
+
+}
