bdc68310cb60310394263f020189e82dc14e492a
wytan12 <tanweiyin12@gmail.com>
2024-01-25 11:22:52 +0800

    update neg bar data

--- EmotionFrontend/src/app/negative-barchart/negative-barchart.component.ts
+++ EmotionFrontend/src/app/negative-barchart/negative-barchart.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { Router } from "@angular/router";
 import { ChartType, ChartDataset, ChartOptions } from "chart.js";
 import * as pluginDataLabels from "chartjs-plugin-datalabels"
+
+import {EmotionService} from "../services/emotion.service";
+import {EmoReadWrite, EmoSurvey} from "../services/emotion";
+import {BaseChartDirective} from "ng2-charts";
+import _default from "chart.js/dist/plugins/plugin.legend";
+import labels = _default.defaults.labels;
 
 @Component({
   selector: 'app-negative-barchart',
@@ -9,9 +15,16 @@ import * as pluginDataLabels from "chartjs-plugin-datalabels"
   styleUrl: './negative-barchart.component.css'
 })
 export class NegativeBarchartComponent implements OnInit {
-  ngOnInit(): void {
-    
-  }
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+  constructor(private router: Router,
+    private emotionService: EmotionService) {}
+
+public data:number[] =[];
+ngOnInit(): void {
+this.getData();
+}
+
+emoSurvey: EmoSurvey[] = [];
   public barChartLabels: string[]= ['Bored', 'Frustrated', 'Anxious'];
 
   // public barChartType: ChartType = "bar";
@@ -27,8 +40,43 @@ export class NegativeBarchartComponent implements OnInit {
   }] as any[];
 
   public barChartData: ChartDataset[] = [
-    {data: [65,59,80] },
+    {data: [1,2,3]},
   ];
+
+  async getData() {
+    const dataHttp = await this.getDataHttp();
+    this.data = dataHttp;
+    console.log(this.data);
+    this.barChartData[0].data= this.data;
+    this.chart?.update();
+  }
+  public getDataHttp(): Promise<number[]> {
+    return new Promise<number[]>(resolve => {
+      const rdata: number[] = [0, 0, 0, 0, 0, 0, 0];
+      let totalEntries = 0;
+
+      this.emotionService.getEmoSurvey().subscribe(emoSurvey => {
+        for (let i = 0; i < emoSurvey.length; i++) {
+          const es: EmoSurvey = emoSurvey[i];
+          rdata[0] += es.Joyful;
+          rdata[1] += es.Curious;
+          rdata[2] += es.Surprised;
+          rdata[3] += es.Confused;
+          rdata[4] += es.Anxious;
+          rdata[5] += es.Frustrated;
+          rdata[6] += es.Bored;
+          totalEntries++;
+        }
+
+         // Calculate average value for each intensity
+        for (let i = 0; i < rdata.length; i++) {
+          rdata[i] /= totalEntries;
+        } 
+
+        resolve(rdata);
+      });
+    });
+  }
 
   public barChartOptions: ChartOptions = {
     responsive: true,
@@ -56,8 +104,6 @@ export class NegativeBarchartComponent implements OnInit {
   //     }
   //   }
   };
-
-  constructor(private router: Router) {}
 
   // public handleChartClick(event: any) {
   //   if (event.active && event.active.length > 0) {
