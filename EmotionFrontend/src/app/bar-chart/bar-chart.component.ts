fb6f382ab101dc6d4b18f44c011c5f2021355e01
bron322 <lebron13145@gmail.com>
2024-03-08 14:35:25 +0800

    changed to 100 days

--- EmotionFrontend/src/app/bar-chart/bar-chart.component.ts
+++ EmotionFrontend/src/app/bar-chart/bar-chart.component.ts
@@ -83,7 +83,7 @@ export class BarChartComponent implements OnInit {
       // Set default values for from and to if not provided
       const defaultFromDate = new Date(); // Default to current date/time
       const defaultToDate = new Date(); // Default to current date/time
-      defaultFromDate.setDate(defaultFromDate.getDate() - 30); // Default to one month ago
+      defaultFromDate.setDate(defaultFromDate.getDate() - 100); // Default to one month ago
       this.getData(defaultFromDate, defaultToDate);
       return; // Exit function to prevent further execution
     }
