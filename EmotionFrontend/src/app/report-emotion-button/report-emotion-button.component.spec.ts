d5c76b1c8dd86ac5e181cdda62799ad284dd3d04
bron322 <lebron13145@gmail.com>
2024-03-23 23:07:50 +0800

    fixed user guide and created button

---
+++ EmotionFrontend/src/app/report-emotion-button/report-emotion-button.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReportEmotionButtonComponent } from './report-emotion-button.component';
+
+describe('ReportEmotionButtonComponent', () => {
+  let component: ReportEmotionButtonComponent;
+  let fixture: ComponentFixture<ReportEmotionButtonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ReportEmotionButtonComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ReportEmotionButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
