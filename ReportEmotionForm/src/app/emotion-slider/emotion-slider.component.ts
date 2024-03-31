58b9753d5c86689c2325548e21cef5ddda0a780e
bron322 <lebron13145@gmail.com>
2024-03-31 23:29:11 +0800

    userID cannot be reflected, timestamp is wrong

--- ReportEmotionForm/src/app/emotion-slider/emotion-slider.component.ts
+++ ReportEmotionForm/src/app/emotion-slider/emotion-slider.component.ts
@@ -1,23 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmotionService } from '../services/emotion.service';
+import { EmoSurvey } from '../services/emotion';
 
 @Component({
   selector: 'app-emotion-slider',
   templateUrl: './emotion-slider.component.html',
   styleUrl: './emotion-slider.component.css'
 })
-export class EmotionSliderComponent {
-  feelingsForm: FormGroup;
+export class EmotionSliderComponent implements OnInit {
 
-  constructor(private formBuilder: FormBuilder) {
+  feelingsForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private emotionService: EmotionService // Inject your EmotionService here
+  ) { }
+
+  ngOnInit(): void {
     this.feelingsForm = this.formBuilder.group({
-      joyful: ['', Validators.required],
-      curious: ['', Validators.required],
-      surprised: ['', Validators.required],
-      confused: ['', Validators.required],
-      anxious: ['', Validators.required],
-      frustrated: ['', Validators.required],
-      bored: ['', Validators.required],
+      Joyful: ['', Validators.required],
+      Curious: ['', Validators.required],
+      Surprised: ['', Validators.required],
+      Confused: ['', Validators.required],
+      Anxious: ['', Validators.required],
+      Frustrated: ['', Validators.required],
+      Bored: ['', Validators.required],
+      Inconducive: ['', Validators.required],
+      Reason: ['', Validators.required],
+      Remarks: [''],
     });
   }
 
@@ -25,6 +36,18 @@ export class EmotionSliderComponent {
     if (this.feelingsForm.valid) {
       console.log('Form submitted successfully!');
       console.log('Form data:', this.feelingsForm.value);
+
+      // Call the addEmoSurvey function from the EmotionService
+      this.emotionService.addEmoSurvey(this.feelingsForm.value).subscribe(EmoSurvey => {
+        console.log('EmoSurvey added successfully!', EmoSurvey.Timestamp);
+      });
+      //   (response: EmoSurvey) => {
+      //     console.log('EmoSurvey added successfully!', response);
+      //   },
+      //   (error) => {
+      //     console.error('Error adding EmoSurvey:', error);
+      //   }
+      // );
     } else {
       console.log('Please answer all compulsory questions.');
     }
---
+++ ReportEmotionForm/src/app/services/message.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class MessageService {
+  messages: string[] = [];
+  add(message: string) {
+    this.messages.push(message);
+  }
+  clear() {
+    this.messages = [];
+  }
+}
