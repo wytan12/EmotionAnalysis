ffaa6b2a17e52d5e20c0362450eb017f2f94771e
ngyh6726 <ngyh6726@gmail.com>
2024-07-01 16:22:50 +0800

    fixed y-axis issue

--- EmotionFrontend/src/app/bar-chart/bar-chart.component.ts
+++ EmotionFrontend/src/app/bar-chart/bar-chart.component.ts
@@ -1,37 +1,39 @@
-import {Component, OnInit, ViewChild} from "@angular/core";
-import { Router } from "@angular/router";
-import {ChartType, ChartDataset, ChartOptions, ChartData} from "chart.js";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { ChartType, ChartDataset, ChartOptions, ChartData } from 'chart.js';
 
-import * as pluginDataLabels from "chartjs-plugin-datalabels"
-import {EmotionService} from "../services/emotion.service";
-import {EmoReadWrite, EmoSurvey} from "../services/emotion";
-import {BaseChartDirective} from "ng2-charts";
-import _default from "chart.js/dist/plugins/plugin.legend";
+import * as pluginDataLabels from 'chartjs-plugin-datalabels';
+import { EmotionService } from '../services/emotion.service';
+import { EmoReadWrite, EmoSurvey } from '../services/emotion';
+import { BaseChartDirective } from 'ng2-charts';
+import _default from 'chart.js/dist/plugins/plugin.legend';
 import labels = _default.defaults.labels;
 
 import { SharedTimeService } from '../shared-time.service';
 import { TitleService } from '../title.service';
 
 @Component({
-  selector: "app-bar-chart",
-  templateUrl: "./bar-chart.component.html",
-  styleUrls: ["./bar-chart.component.css"]
+  selector: 'app-bar-chart',
+  templateUrl: './bar-chart.component.html',
+  styleUrls: ['./bar-chart.component.css'],
 })
 export class BarChartComponent implements OnInit {
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
-  
-  constructor(private router: Router,
-              private emotionService: EmotionService,
-              private sharedTimeService: SharedTimeService,
-              private titleService: TitleService) {}
 
-  public data:number[] =[];
+  constructor(
+    private router: Router,
+    private emotionService: EmotionService,
+    private sharedTimeService: SharedTimeService,
+    private titleService: TitleService
+  ) {}
 
-  public title:string ='';
+  public data: number[] = [];
+
+  public title: string = '';
 
   emoSurvey: EmoSurvey[] = [];
-  
-  public barChartLabels: string[]= [
+
+  public barChartLabels: string[] = [
     'Joyful',
     'Curious',
     'Surprised',
@@ -44,33 +46,39 @@ export class BarChartComponent implements OnInit {
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
 
-  public barChartPlugins = [{
-    datalabels: {
-      anchor: 'end',
-      align: 'end',
-    }
-  }] as any[];
+  public barChartPlugins = [
+    {
+      datalabels: {
+        anchor: 'end',
+        align: 'end',
+      },
+    },
+  ] as any[];
 
   public barChartData: ChartDataset[] = [
-    {data: [], label: 'Average emotion intensity', backgroundColor: 'rgba(0, 0, 200, 0.5)'},
+    {
+      data: [],
+      label: 'Average emotion intensity',
+      backgroundColor: 'rgba(0, 0, 200, 0.5)',
+    },
   ];
 
   ngOnInit(): void {
-      // this.getData();
-      this.sharedTimeService.selectedTime$.subscribe((timeRange: number[]) => {
-        if (timeRange && timeRange.length === 2) {
-          const from = new Date(timeRange[0]);
-          console.log("From Date: ", from)
-          const to = new Date(timeRange[1]);
-          console.log("To Date: ", to)
-          this.getData(from, to);
-        } else{
-          this.getData(undefined, undefined);
-        }
-      });
-      this.sharedTimeService.selectedTime$.subscribe((timeRange: number[]) => {
-        console.log('Selected time range:', timeRange);
-      });
+    // this.getData();
+    this.sharedTimeService.selectedTime$.subscribe((timeRange: number[]) => {
+      if (timeRange && timeRange.length === 2) {
+        const from = new Date(timeRange[0]);
+        console.log('From Date: ', from);
+        const to = new Date(timeRange[1]);
+        console.log('To Date: ', to);
+        this.getData(from, to);
+      } else {
+        this.getData(undefined, undefined);
+      }
+    });
+    this.sharedTimeService.selectedTime$.subscribe((timeRange: number[]) => {
+      console.log('Selected time range:', timeRange);
+    });
   }
 
   ngAfterViewInit() {
@@ -93,18 +101,18 @@ export class BarChartComponent implements OnInit {
     const dataHttp = await this.getDataHttp(from, to);
     this.data = dataHttp;
     console.log(this.data);
-    this.barChartData[0].data= this.data;
+    this.barChartData[0].data = this.data;
     if (this.chart) {
       this.chart.update();
     }
   }
 
   public getDataHttp(from: Date, to: Date): Promise<number[]> {
-    return new Promise<number[]>(resolve => {
+    return new Promise<number[]>((resolve) => {
       const rdata: number[] = [0, 0, 0, 0, 0, 0, 0];
       let totalEntries = 0;
 
-      this.emotionService.getEmoSurvey().subscribe(emoSurvey => {
+      this.emotionService.getEmoSurvey().subscribe((emoSurvey) => {
         for (let i = 0; i < emoSurvey.length; i++) {
           const es: EmoSurvey = emoSurvey[i];
           const timestampnumber = es['Timestamp'];
@@ -121,10 +129,10 @@ export class BarChartComponent implements OnInit {
           }
         }
 
-         // Calculate average value for each intensity
+        // Calculate average value for each intensity
         for (let i = 0; i < rdata.length; i++) {
           rdata[i] /= totalEntries;
-        } 
+        }
 
         resolve(rdata);
       });
@@ -138,36 +146,36 @@ export class BarChartComponent implements OnInit {
       x: {
         ticks: {
           font: {
-            size: 16
-          }
-        }
+            size: 16,
+          },
+        },
       },
       y: {
         beginAtZero: true,
         ticks: {
           font: {
-            size: 18
+            size: 18,
           },
           stepSize: 0.5,
-        }
-      }
+        },
+      },
     },
     plugins: {
       legend: {
         labels: {
           font: {
-            size: 15
-          }
-        }
-      }
-    }
+            size: 15,
+          },
+        },
+      },
+    },
   };
 
   public handleChartClick(event: any) {
     if (event.active && event.active.length > 0) {
       const clickedLabel = event.active[0];
-      const value = this.barChartLabels[clickedLabel.index]
-      
+      const value = this.barChartLabels[clickedLabel.index];
+
       // this.title = value;
       this.titleService.selectedTitle = value;
       console.log(value);
@@ -175,5 +183,3 @@ export class BarChartComponent implements OnInit {
     }
   }
 }
-
-
