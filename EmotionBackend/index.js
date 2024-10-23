a542b562f3096616c83549fa6b62a8f83059280b
wytan12 <tanweiyin12@gmail.com>
2024-10-23 11:53:53 +0100

    setup nginx for emotion_form

--- EmotionBackend/index.js
+++ EmotionBackend/index.js
@@ -19,11 +19,11 @@ app.use("/api", APIrouter);
 
 ///////////////////////////////////////////////// cors set-up //////////////////////////////////////////////////
 var corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ['http://localhost:80', 'http://localhost:60312'],
   credentials: true,
   optionsSuccessStatus: 200,
   headers: {
-    "Access-Control-Allow-Origin": "http://localhost:5173", // Allow CORS from any origin
+    "Access-Control-Allow-Origin": "http://localhost:60312", // Allow CORS from any origin
     "Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, DELETE, OPTIONS", // Allow all HTTP methods
     "Access-Control-Allow-Headers": "*", // Allow specified headers
   },
